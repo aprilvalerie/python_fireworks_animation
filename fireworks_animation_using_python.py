2efe971b31368230a4f38ea7da3517e167cd971e
aprilvalerie <punzalanaprilvalerie@gmail.com>
2026-03-02 16:08:18 +0800

    finished event loop function

--- fireworks_animation_using_python.py
+++ fireworks_animation_using_python.py
@@ -8,3 +8,30 @@ pygame.display.set_caption("Fireworks!")
 
 FPS = 60
 
+colors = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (0, 255, 255),
+    (255, 165, 0),
+    (255, 255, 255),
+    (230, 230, 250),
+    (255, 192, 203)
+]
+def main():
+     run = True
+     clock = pygame.time.Clock()
+
+     while run:
+         clock.tick(FPS)
+
+         for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 run = False
+                 break
+
+if __name__ == '__main__':
+    main()
+
+    
+
